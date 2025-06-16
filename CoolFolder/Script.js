fa2a2a83ea4b5a598bac817468e860cc284d2751
ThunderGam3z <jcbwalk11@gmail.com>
2025-06-15 21:31:03 -0400

    Add files via upload

---
+++ CoolFolder/Script.js
@@ -0,0 +1,8 @@
+
+
+function vBurks() {
+    for (let i=0; i > 500 ;i++) {
+        document.getElementById("Count").innerText = "Generated: " + i + " Vburks!";
+        console.log('vburks');
+    }
+}
